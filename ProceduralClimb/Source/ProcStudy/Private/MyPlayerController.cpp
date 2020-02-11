124b97bb81bc4b90bdba21f0829af1a7161bd788
Jeroo97 <jeroo1997@gmail.com>
2020-02-11 12:11:08 -0500

    -Created the new character system ready for the GHaSt.

--- ProceduralClimb/Source/ProcStudy/Private/MyPlayerController.cpp
+++ ProceduralClimb/Source/ProcStudy/Private/MyPlayerController.cpp
@@ -5,22 +5,23 @@
 #include "Engine/World.h"
 #include "CollisionQueryParams.h"
 #include "DrawDebugHelpers.h"
+#include "Camera/CameraComponent.h"
 
 void AMyPlayerController::Tick(float DeltaTime)
 {
 	FVector HitPosition;
-	RayCast(HitPosition);
+	//RayCast(HitPosition);
 	UE_LOG(LogTemp, Warning, TEXT("The hit location is: %s"), *HitPosition.ToString());
 }
 
 void AMyPlayerController::RayCast(FVector& HitLocation)
 {
 	FHitResult HitResult;
-	auto ForwardVector = GetPawn()->GetActorForwardVector();
+	auto ForwardVector = GetPawn()->FindComponentByClass<UCameraComponent>()->GetForwardVector();
 	auto StartLocation = GetPawn()->GetActorLocation();
 	auto EndLocation = StartLocation + (ForwardVector * LineTraceRange);
 
-	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Green, false, 1, 0, 1);
+	//DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Green, false, 1, 0, 1);
 
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(GetPawn());
